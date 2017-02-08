e901d9dd62d65b391fa256fe168f4b2eb29642da
Diky Arga <dikyarga.id@gmail.com>
2017-02-08 15:36:08 +0700

    FINAL Release

--- todo.js
+++ todo.js
@@ -74,6 +74,92 @@ class Todo {
       console.log(`${this.todoList[i].id} [ ${this.todoList[i].status == true ? 'X' : ' '} ] ${this.todoList[i].title} `);
     }
   }
+
+  outstanding(order){
+    let todoList = []
+    for (var i = 0; i < this.todoList.length; i++) {
+      if (this.todoList[i].status == false) {
+        todoList.push(this.todoList[i])
+      }
+    }
+
+    this.showOrderBy(todoList, order)
+  }
+
+  completed(order){
+    let todoList = []
+    for (var i = 0; i < this.todoList.length; i++) {
+      if (this.todoList[i].status == true) {
+        todoList.push(this.todoList[i])
+      }
+    }
+    this.showOrderBy(todoList, order)
+  }
+
+  showOrderBy(todoList, order){
+    if (order == 'asc') {
+      for (var i = 0; i < todoList.length; i++) {
+        console.log(`${todoList[i].id} [ ${todoList[i].status == true ? 'X' : ' '} ] ${todoList[i].title} `);
+
+      }
+    } else {
+      for (var i = todoList.length -1; i >= 0 ; i--) {
+        console.log(`${todoList[i].id} [ ${todoList[i].status == true ? 'X' : ' '} ] ${todoList[i].title} `);
+      }
+    }
+  }
+
+  sortingByName(todoList){
+    let todos = todoList.sort(function(a, b){
+      var titleA = a.title.toUpperCase(); // ignore upper and lowercase
+      var titleB = b.title.toUpperCase(); // ignore upper and lowercase
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+      // names must be equal
+      return 0;
+    })
+  }
+
+  addTag(id, arrTag){
+    let found = false
+    let indexTodo = 0
+    for (var i = 0; i < this.todoList.length; i++) {
+      if (this.todoList[i].id == id) {
+        this.todoList[i].tag = arrTag
+        indexTodo = i
+        found = true
+      }
+    }
+    if (found) {
+      this.writeToFile()
+      return {status: true, todo: this.todoList[indexTodo]}
+    } else{
+      return false
+    }
+  }
+
+  filterByTag(tag){
+    // console.log(tag);
+    for (let i = 0; i < this.todoList.length; i++) {
+      for (let j = 0; j < this.todoList[i].tags.length; j++) {
+        if (this.todoList[i].tags[j].toLowerCase() == tag.toLowerCase()) {
+          console.log(`${this.todoList[i].id} [ ${this.todoList[i].status == true ? 'X' : ' '} ] ${this.todoList[i].title} `);
+        }
+      }
+    }
+  }
+
+  sortingByDate(todoList){
+    let todos = todoList.sort(function(a, b){
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    })
+    return todos
+  }
+
 }
 
 class Command{
@@ -131,6 +217,60 @@ class Command{
           console.log('-------- Berikut Todo List kamu ----------');
           todo.showList()
           break
+        case 'list:outstanding':
+          console.log('-------- Berikut Todo List yang belum kamu kelarin ----------');
+          if (this.arguments[1] == undefined) {
+            console.log('Kita urutkan berdasarkan waktu masukin ya!');
+            todo.outstanding('asc')
+          } else {
+            if (this.arguments[1] == 'asc') {
+              console.log('Kita urutkan berdasarkan waktu masukin ya!');
+              todo.outstanding('asc')
+            } else {
+              console.log('Urutanya dibalik dari waktu masukin ya!');
+              todo.outstanding('desc')
+            }
+          }
+          break
+        case 'list:completed':
+          console.log('-------- Berikut Todo List udah kamu kelarin ----------');
+          if (this.arguments[1] == undefined) {
+            console.log('Kita urutkan berdasarkan waktu masukin ya!');
+            todo.completed('asc')
+          } else {
+            if (this.arguments[1] == 'asc') {
+              console.log('Kita urutkan berdasarkan waktu masukin ya!');
+              todo.completed('asc')
+            } else {
+              console.log('Urutanya dibalik dari waktu masukin ya!');
+              todo.completed('desc')
+            }
+          }
+          break
+        case 'tag':
+          if (this.arguments[1] == undefined) {
+            console.log('Task mana yang mau kamu tambahin tag, Sis ?. Tambahin setelah complete ya!');
+          } else {
+            let arrTag = []
+            for (var i = 2; i < this.arguments.length; i++) {
+              arrTag.push(this.arguments[i])
+            }
+            // console.log(arrTag);
+            let resultAddTag = todo.addTag(this.arguments[1], arrTag)
+            if (resultAddTag.status) {
+              console.log(`Tagged task ${resultAddTag.todo.title} with ${arrTag.join(' , ')}`);
+            } else {
+              console.log('Tagging fail ): ');
+            }
+          }
+          break
+        // case 'filter':
+        //   if (this.arguments[1] == undefined) {
+        //     console.log('Apa nama tag-nya, Sis ?');
+        //   } else {
+        //
+        //   }
+        //   break
         case 'complete':
           if (this.arguments[1] == undefined) {
             console.log('Task mana yang mau di complete-in, Sis ?. Tambahin setelah complete ya!');
@@ -154,6 +294,10 @@ class Command{
           }
           break
         default:
+          let argFilter = this.arguments[0].split(':')
+          if (argFilter[0] == 'filter') {
+            todo.filterByTag(argFilter[1])
+          }
 
       }
     }
@@ -169,11 +313,15 @@ class Command{
     console.log('----- Ada yang bisa di bantu, Sis ? ----- ');
     console.log(`$ node todo.js help - Jika kamu butuh bantuan`);
     console.log('$ node todo.js list - tampilih semua todo list');
+    console.log('$ node todo.js list:outstanding <asc || desc>- tampilih semua todo list yang belum dikerjain');
+    console.log('$ node todo.js list:completed <asc || desc>- tampilih semua todo list yang udah dikejain');
+    console.log('$ node todo.js filter:<tag-name> - tampilih semua todo list dengan tag tersebut');
     console.log('$ node todo.js add <task_content> - tambahin todo list');
     console.log('$ node todo.js task <id-todo> - tampilin dengan index ke');
     console.log('$ node todo.js delete <id-todo> - delete todo dengan index ke');
     console.log('$ node todo.js complete <id-todo> - complete task index ke');
     console.log('$ node todo.js uncomplete <id-todo> - batal complete task dengan index ke');
+    console.log('$ node todo.js tag <id-todo> <tags> <tags> - tambah tag ke dalama todo dengan index ke');
   }
 }
 
